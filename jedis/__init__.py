ce6b299a9424f005bdf611c1e687818feeceed0c
Kobsser <82896234+Kobsser@users.noreply.github.com>
2022-05-30 23:17:10 +0430

    v0.1.0

---
+++ jedis/__init__.py
@@ -0,0 +1,13 @@
+# Jedis
+# t.me/Kobsser
+
+__version__ = "0.1.0"
+__author__ = "Kobsser"
+__copyright__ = "Copyright (C) 2022 Kobsser"
+__description__ = "t.me/Kobsser"
+
+from .jedis import Jedis, load, dump
+
+__all__ = [
+    Jedis, load, dump
+]
